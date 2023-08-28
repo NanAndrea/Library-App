695bbf104577760579bacf3825900476553d8088
Nan Andrea <balajelandrea@yahoo.com>
2023-08-28 13:44:28 +0300

    added notFound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,16 @@
 import { Box, Button, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import Image from "../img/page-not-found.jpg";
 
 export default function(){
     return(
-        <Box minHeight={"100vh"}>
-            <Typography>404</Typography>
-            <Button variant="contained" size="large" component={NavLink}
+        <Box minHeight={"100vh"} sx={{display:"flex", justifyContent:"flex-start", alignItems:"flex-end", backgroundImage: `url(${Image})`, backgroundPosition: "center",backgroundRepeat: 'no-repeat', 
+        backgroundSize: "cover"}} >
+            <Box sx={{}}>
+            <Button variant="text" size="large" component={NavLink} sx={{ color:"#B65866"}}
                 to={"/"} pad={5}>GO TO HOMEPAGE</Button>
+            </Box>
+            
         </Box>
     )
 
